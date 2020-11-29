27ffe553200279f00aafd12e790c62d7234e539b
Michał Romanowski <michal.romanowski87@gmail.com>
2020-11-29 11:51:32 +0100

    Move them back to xml attributes.

--- Simulation/Parameters/OptimizationParameters.cs
+++ Simulation/Parameters/OptimizationParameters.cs
@@ -9,24 +9,44 @@ namespace Simulations.Parameters
     [Serializable]
     public class OptimizationParameters : IPopulationParameters
     {
+        [XmlAttribute]
         public int CurrentEra { get; set; }
+        [XmlAttribute]
         public int NumberOfEras { get; set; }
+        [XmlAttribute]
         public int TurnsOfHerding { get; set; }
+        [XmlAttribute]
         public double TargetFitness { get; set; }
+        [XmlAttribute]
         public int NumberOfParticipants { get; set; }
+        [XmlAttribute]
         public double MutationPower { get; set; }
+        [XmlAttribute]
         public ESheepType SheepType { get; set; }
+        [XmlAttribute]
         public EPerceptionType PerceptionType { get; set; }
+        [XmlAttribute]
         public int NumberOfSeenShepherds { get; set; }
+        [XmlAttribute]
         public int NumberOfSeenSheep { get; set; }
+        [XmlAttribute]
         public int NumberOfHiddenLayers { get; set; }
+        [XmlAttribute]
         public int NumberOfNeuronsInHiddenLayer { get; set; }
+        [XmlAttribute]
         public int PopulationSize { get; set; }
+        [XmlAttribute]
         public EFitnessType FitnessType { get; set; }
+        [XmlAttribute]
         public bool NotIdenticalAgents { get; set; }
+
+        [XmlAttribute]
         public bool RandomPositions { get; set; }
+        [XmlAttribute]
         public int NumberOfRandomSets { get; set; }
+        [XmlAttribute]
         public int SeedForRandomSheepForBest { get; set; }
+        [XmlAttribute]
         public bool RandomizeNeuralNetOnInit { get; set; }
         [XmlElement]
         public RandomSetsList RandomSetsForBest { get; set; }
